c6b5b89d1dfa4eb4e454297f417dc18c3a16c6df
ogtife <142328283+ogtife@users.noreply.github.com>
2023-08-15 11:56:08 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,26 +7,26 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
+global.Owner = ['2347032983433']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['2347032983433']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['2347032983433']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.BotName = "TIFE-MD"
+global.packname = "TIFE-MD"
+global.author = "By: TIFE"
+global.OwnerName = "TIFE"
+global.BotSourceCode = "https://github.com/ogtife/Miku-MD" // -- Please don't change -- //
+global.SupportGroupLink = "https://chat.whatsapp"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = ['.'] // Prefix ---  change it to anythong you want except '@' //
 
 
 global.location = "West Bengal, India"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
-global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
+global.vidmenu = { url: '' }
 global.websitex = "https://github.com/FantoX001"
 global.lolhuman = "KaysaS"
 
